4dbc22804906bdaaa4ab7eb9ff35c49252708163
Timothy Baker <tbake0155@gmail.com>
2018-02-23 12:22:37 -0500

    added files without gitnotes notes

--- machine_precision_inline_notes.py
+++ machine_precision_inline_notes.py
@@ -2,16 +2,19 @@
 
 import sys
 
+#This function contains a standard algorithm to find the floating point decimal limit for a machine
+#visit https://en.wikipedia.org/wiki/Machine_epsilon for more information
 def machinePrecision() :
     a = 4.0 / 3.0
     b = a - 1.0
     c = b + b + b
-    precision = abs(c - 1.0)
+    precision = abs(c - 1.0) # Value for the machine precision determined by machinePrecision()
 
     return precision
 
+#This function is the first call when the program is executed
 def main() :
-    if len(sys.argv) < 2 :
+    if len(sys.argv) < 2 : # System libray call that allows access to the passed command line arguments
         precision = machinePrecision()
         print(precision)
     else :
